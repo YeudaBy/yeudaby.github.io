a22fef4c5221d12482b7fad2a0b9c36db1532fce
YeudaBy <YeudaBorodyanski@gmail.com>
2022-07-29 11:36:24 +0300

    try again

---
+++ _next/static/znfLrMAxNvJSWliDc2uX2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ada07874937f93a7.css","static/chunks/pages/index-3ffca98086b9acd5.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
